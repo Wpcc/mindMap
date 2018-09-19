f7fe10e25cb0bf3ee90f385b41a03d6c5a2b6b85
Wpcc <qiaofangzidemao@gmail.com>
2018-09-19 18:13:17 +0800

    响应式布局中的banner完成

--- responsive-layout/js/index.js
+++ responsive-layout/js/index.js
@@ -1,3 +1,27 @@
 /**
  * Created by zhousg on 2017/10/16.
  */
+$(function(){
+  banner();
+});
+var banner = function(){
+  // 获取轮播图数据  ==>  ajax
+  // 根据数据动态渲染  ==>  判断屏幕宽度
+  // 运用模板引擎进行渲染
+  // 测试功能
+
+  $.ajax({
+    type:'get',
+    url:'lib/data.json',
+    dataType:'json',
+    success:function(data){
+      var isMob = true;
+      $(window).width() < 768 ? isMob = true : false;
+
+      console.log(isMob);
+    },
+    error:function(){
+
+    }
+  })
+}
